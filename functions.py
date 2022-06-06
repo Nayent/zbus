1460fcf9052c61b6fb2c4ca75360c60e2a4b1ddf
Guilherme Nomura <guilherme.leite@sensedata.com.br>
2022-06-06 00:52:04 -0300

    store csv

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import numpy as np
 
+from model import de_para
+
 qnt_bus = 37
 
 
@@ -64,7 +66,6 @@ def tipo_3(bus2, bus1, bus, z):
 
 
 def kron_reduc(zbus):
-
     zbus = (
         zbus[:qnt_bus, :qnt_bus] -
         zbus[:qnt_bus, qnt_bus:].dot(
@@ -75,3 +76,57 @@ def kron_reduc(zbus):
     )
 
     return zbus
+
+
+def model_bus(data):
+    bus = np.zeros((qnt_bus, qnt_bus))
+    already_bars = set()
+    file_aux = 0
+
+    for data_ in data:
+        for index, row in data_.iterrows():
+            if row.get('Barras tipo 1'):
+                num_bar = de_para[int(row['Barras tipo 1'])]['n_bar']
+                bus = tipo_1(num_bar, bus, row['X+'])
+                already_bars.add(int(row['Barras tipo 1']))
+            elif int(row['De']) in already_bars and int(row['Para']) in already_bars:
+                num_bar1 = de_para[int(row['De'])]['n_bar']
+                num_bar2 = de_para[int(row['Para'])]['n_bar']
+                bus = tipo_3(num_bar1, num_bar2, bus, row['X+  pu'])
+                bus_zero = tipo_3(num_bar1, num_bar2, bus_zero, row['X0  pu'])
+            else:
+                if int(row['De']) in already_bars:
+                    num_bar1 = de_para[int(row['De'])]['n_bar']
+                    num_bar2 = de_para[int(row['Para'])]['n_bar']
+                else:
+                    num_bar1 = de_para[int(row['Para'])]['n_bar']
+                    num_bar2 = de_para[int(row['De'])]['n_bar']
+                bus = tipo_2(num_bar2, num_bar1, bus, row['X+  pu'])
+                bus_zero = tipo_2(num_bar2, num_bar1, bus_zero, row['X0  pu'])
+                already_bars.add(int(row['De']))
+                already_bars.add(int(row['Para']))
+
+        file_aux += 1
+        if file_aux == 1:
+            bus_zero = bus
+
+    np.savetxt("bus_postiva.csv", bus, delimiter="|")
+    np.savetxt("bus_zero.csv", bus_zero, delimiter="|")
+    return bus, bus_zero
+
+
+# Faltas
+def equilibrado(zbus, ref_bus):
+    pass
+
+
+def monofasica(zbus, ref_bus):
+    pass
+
+
+def bifasica(zbus, ref_bus):
+    pass
+
+
+def bifasica_terra(zbus, ref_bus):
+    pass
--- matriz_zbus.py
+++ matriz_zbus.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from model import de_para
 from functions import (
-    get_file, tipo_1, tipo_2, tipo_3
+    get_file, model_bus
 )
 
 def main():
@@ -14,11 +14,9 @@ def main():
 
     data = get_file()
 
-    qnt_bus = 37
-
     code = time.time()
 
-    final_bus = model_bus(data, qnt_bus)
+    pos_neg, zero = model_bus(data)
 
     end = time.time()
 
@@ -26,42 +24,6 @@ def main():
 
     print(f'\nExecution Time without reading_file = {end - code} seconds')
     print(f'\nExecution Time with reading_file = {end - start} seconds\n')
-
-
-def model_bus(data, qnt_bus):
-    bus = np.zeros((qnt_bus, qnt_bus))
-    already_bars = set()
-    file_aux = 0
-
-    for data_ in data:
-        for index, row in data_.iterrows():
-            if row.get('Barras tipo 1'):
-                num_bar = de_para[int(row['Barras tipo 1'])]['n_bar']
-                bus = tipo_1(num_bar, bus, row['X+'])
-                already_bars.add(int(row['Barras tipo 1']))
-            elif int(row['De']) in already_bars and int(row['Para']) in already_bars:
-                num_bar1 = de_para[int(row['De'])]['n_bar']
-                num_bar2 = de_para[int(row['Para'])]['n_bar']
-                bus = tipo_3(num_bar1, num_bar2, bus, row['X+  pu'])
-                bus_zero = tipo_3(num_bar1, num_bar2, bus_zero, row['X0  pu'])
-            else:
-                if int(row['De']) in already_bars:
-                    num_bar1 = de_para[int(row['De'])]['n_bar']
-                    num_bar2 = de_para[int(row['Para'])]['n_bar']
-                else:
-                    num_bar1 = de_para[int(row['Para'])]['n_bar']
-                    num_bar2 = de_para[int(row['De'])]['n_bar']
-                bus = tipo_2(num_bar2, num_bar1, bus, row['X+  pu'])
-                bus_zero = tipo_2(num_bar2, num_bar1, bus_zero, row['X0  pu'])
-                already_bars.add(int(row['De']))
-                already_bars.add(int(row['Para']))
-
-        file_aux += 1
-        if file_aux == 1:
-            bus_zero = bus
-    import ipdb
-    ipdb.set_trace()
-    return bus
 
 
 if __name__ == '__main__':
